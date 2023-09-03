af368e09cf0a9d542b5785a035e3b7217cff8830
SeJun Bae <sbae68@gatech.edu>
2023-09-02 21:37:53 -0700

    New Configs.

---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+
+-- Tab
+vim.opt.tabstop = 4                 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4             -- number of spaces in tab when editing
+vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
+vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+
+-- UI config
+vim.opt.number = true               -- show absolute number
+vim.opt.relativenumber = true
+vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+vim.opt.swapfile = false
