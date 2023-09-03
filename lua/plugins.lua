f6c88db11ad431dc2f71a885bca83966b1e5f90a
SeJun Bae <sejun.bae@outlook.com>
2023-09-02 22:11:13 -0700

    Add LSP support.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,8 +9,29 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
+
   }
 
+  -- Plugin for LSP
   use 'folke/tokyonight.nvim'
 
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+      -- LSP Support
+ }
+  use {
+      'neovim/nvim-lspconfig',             -- Required
+      'williamboman/mason.nvim',           -- Optional
+      'williamboman/mason-lspconfig.nvim', -- Optional
+
+      -- Autocompletion
+      'hrsh7th/nvim-cmp',     -- Required
+      'hrsh7th/cmp-nvim-lsp', -- Required
+      'L3MON4D3/LuaSnip',     -- Required
+ 
+  }
+
 end)
+
+
